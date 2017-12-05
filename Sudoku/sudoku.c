cf679b7d5e255486fcf01bf5537c27101108d489
mdemo <mdemo@10.250.52.201>
2017-12-04 16:12:14 -0800

    initial commit for c file

---
+++ Sudoku/sudoku.c
@@ -0,0 +1,17 @@
+
+/* 
+ * File:   sudoku.c
+ * Authors: mdemong, gareiner
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World! :D");
+    return (EXIT_SUCCESS);
+}
+
